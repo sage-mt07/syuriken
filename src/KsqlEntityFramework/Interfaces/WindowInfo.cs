305bc20d60541c7b5101459f9b5f057e4bbfe5eb
sage-mt07 <65053888+sage-mt07@users.noreply.github.com>
2025-03-22 18:46:36 +0900

    Create WindowInfo.cs

---
+++ src/KsqlEntityFramework/Interfaces/WindowInfo.cs
@@ -0,0 +1,12 @@
+public class WindowInfo
+    {
+        /// <summary>
+        /// ウィンドウの開始時間
+        /// </summary>
+        public DateTimeOffset Start { get; set; }
+
+        /// <summary>
+        /// ウィンドウの終了時間
+        /// </summary>
+        public DateTimeOffset End { get; set; }
+    }
