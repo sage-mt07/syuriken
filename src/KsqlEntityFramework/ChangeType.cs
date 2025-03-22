c7f50882f9c55d65934f4d7ae43520ae02fa4866
sage-mt07 <65053888+sage-mt07@users.noreply.github.com>
2025-03-22 18:28:21 +0900

    Create ChangeType.cs

---
+++ src/KsqlEntityFramework/ChangeType.cs
@@ -0,0 +1,17 @@
+ public enum ChangeType
+    {
+        /// <summary>
+        /// 挿入
+        /// </summary>
+        Insert,
+
+        /// <summary>
+        /// 更新
+        /// </summary>
+        Update,
+
+        /// <summary>
+        /// 削除
+        /// </summary>
+        Delete
+    }
